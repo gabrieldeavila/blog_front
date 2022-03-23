e246a1656090fd40f6e1a7e1246119b7b97144a8
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-03-22 21:09:01 -0300

    Criando básico do gerenciamento com redux

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div>
+        <p>Home</p>
+      </div>
+      <div>
+        <p>Criar Post</p>
+      </div>
+    </div>
+  );
+}
