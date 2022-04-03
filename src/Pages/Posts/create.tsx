3a71d361e3124d1a6e62e81e0c981058abd642ee
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-02 21:01:41 -0300

    new line

--- src/Pages/Posts/create.tsx
+++ src/Pages/Posts/create.tsx
@@ -3,10 +3,17 @@ import React from "react";
 export default function Create() {
   return (
     <div className="posts-create">
-      <div className="post-name">
+      <div className="post-input">
         <p>Nome do Post:</p>
-        <input type="text" />
+        <input required type="text" />
       </div>
+
+      <div className="post-input">
+        <p>Descrição:</p>
+        <input required type="text" />
+      </div>
+
+      <button>Salvar</button>
     </div>
   );
 }
