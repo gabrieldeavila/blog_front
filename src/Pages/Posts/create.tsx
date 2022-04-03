245c386bb65105676b758303f0c48ac5eb99c0cd
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-03 20:21:03 -0300

    Finalização pg create

--- src/Pages/Posts/create.tsx
+++ src/Pages/Posts/create.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addPost } from "../../redux/effects/Posts";
 
 export default function Create() {
+  const dispatch = useDispatch();
+
+  const title = useRef("");
+  const description = useRef("");
+
+  const handleSave = () => {
+    dispatch(addPost({ title: title.current, text: description.current }));
+  };
+
   return (
     <div className="posts-create">
       <div className="post-input">
         <p>Nome do Post:</p>
-        <input required type="text" />
+        <div className="post-wrapper">
+          <input
+            required
+            type="text"
+            onChange={(e) => (title.current = e.target.value)}
+          />
+        </div>
       </div>
 
       <div className="post-input">
         <p>Descrição:</p>
-        <input required type="text" />
+        <div className="post-wrapper">
+          <textarea
+            onChange={(e) => (description.current = e.target.value)}
+            required
+          />
+        </div>
       </div>
 
-      <button>Salvar</button>
+      <button className="save-button" onClick={handleSave}>
+        Salvar
+      </button>
     </div>
   );
 }
